bc7d731e092724f4d5873c6a2e1d8d9d3ea694d8
KenjiAnderson <prometheus0730@outlook.com>
2022-11-13 02:50:20 +0530

    update

--- frontend/src/Components/Cartmisc.jsx
+++ frontend/src/Components/Cartmisc.jsx
@@ -1,8 +1,9 @@
 import { Box, Button, Divider, Input, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-import styles from "./styles.module.css";
+import { Navigate, useNavigate } from "react-router-dom";
 
-const Cartmisc = () => {
+const Cartmisc = ({subtotal=0}) => {
+  const navigate = useNavigate()
   return (
     <>    <Box w={"90%"} m="auto" border={"1px solid white"} mt="10px">
       <VStack align={"left"}>
@@ -11,30 +12,33 @@ const Cartmisc = () => {
         <Divider />
       </VStack>
 
-      <Box className={styles.section3}>
-        <Box className={styles.section31}>
+      <Box display={"flex"} pt="15px" >
+        <Box display={"flex"} flexDirection="column" gap={"25px"} w="50%" >
           <Text fontSize={'14px'}>
             Use code WELCOME 10 for 10% off selected items when your first order
             is over ₹600
           </Text>
-          <Box className={styles.section31a}>
+          <Box display={"flex"}>
             <Input placeholder="Gift Card/Store Credit/Promo Code" />
             <Button colorScheme={"gray"} fontSize="13px">USE CODE</Button>
           </Box>
         </Box>
         <Box
-          className={styles.section32}
+          w={"50%"}
+          display="50%"
+          flexDirection={"column"}
+          gap="5px"
           align="right"
           border="1px solid white"
         >
           <Text fontSize={14.5} color={"gray"}>
-            Subtotal {"Subtotal ₹ 3275"}
+            Subtotal {subtotal.toLocaleString()}
           </Text>
           <Text fontSize={14.5} color={"gray"}>
             Shipping(DHL Express) {"₹ 0.00"}
           </Text>
           <Text fontSize={14.5} fontWeight={600}>
-            Grand Total {"₹ 0.00"}
+            Grand Total {subtotal.toLocaleString()}
           </Text>
           <Text fontSize={14.5} color={"gray"}>
             VAT excetption. VAT not included. Shipping not included.
@@ -54,6 +58,8 @@ const Cartmisc = () => {
           alignItems={"center"}
           justifyContent={"center"}
           w="170px"
+          cursor={"pointer"}
+           onClick={()=>navigate("/cart/delivery")}
         >
           PROCEED TO CHECKOUT
         </Box>
